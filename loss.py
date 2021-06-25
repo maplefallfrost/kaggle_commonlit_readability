7fa36f48bc45baceb68de28dec276ed973b326fa
Ye Junyu <maplefallfrost@gmail.com>
2021-06-25 19:25:48 +0800

    refactor and add Gaussian js loss to train self distill model

--- loss.py
+++ loss.py
@@ -13,36 +13,49 @@ class LossWrapper:
         if self.loss_name == 'MSE':
             pred_label = output_dict[label_name]
             ground_truth_label = collate_batch[label_name]
-            return MSELoss(pred_label, ground_truth_label)
+            return MSE_loss(pred_label, ground_truth_label)
         
         if self.loss_name == 'RMSE':
             pred_label = output_dict[label_name]
             ground_truth_label = collate_batch[label_name]
-            return RMSELoss(pred_label, ground_truth_label)
+            return RMSE_loss(pred_label, ground_truth_label)
         
         if self.loss_name == 'dist':
             pred_mean = output_dict[label_name].squeeze()
             standard_error_name = "_".join([dataset_name, "standard_error"])
             pred_std = output_dict[standard_error_name].squeeze()
             pred_std = torch.exp(pred_std)
-            ground_truth_mean = collate_batch[label_name].squeeze()
-            ground_truth_std = collate_batch[standard_error_name].squeeze()
-            ground_truth_std[ground_truth_std < std_lower_bound] = std_lower_bound
-            p = torch.distributions.Normal(pred_mean, pred_std)
-            q = torch.distributions.Normal(ground_truth_mean, ground_truth_std)
-            dist_loss = torch.distributions.kl_divergence(p, q)
-            return torch.mean(dist_loss)
+            gt_mean = collate_batch[label_name].squeeze()
+            gt_std = collate_batch[standard_error_name].squeeze()
+            gt_std[gt_std < std_lower_bound] = std_lower_bound
+            return Gaussian_kl_loss(pred_mean, pred_std, gt_mean, gt_std)
 
 
-def MSELoss(logits, labels):
+def MSE_loss(logits, labels):
     loss_fn = torch.nn.MSELoss()
     logits = logits.view(-1).to(labels.dtype)
     loss = loss_fn(logits, labels.view(-1))
     return loss
 
 
-def RMSELoss(logits, labels):
+def RMSE_loss(logits, labels):
     loss_fn = torch.nn.MSELoss()
     logits = logits.view(-1).to(labels.dtype)
     loss = torch.sqrt(loss_fn(logits, labels.view(-1)))
     return loss
+
+
+def Gaussian_kl_loss(pred_mean, pred_std, gt_mean, gt_std):
+    p = torch.distributions.Normal(pred_mean, pred_std)
+    q = torch.distributions.Normal(gt_mean, gt_std)
+    kl_loss = torch.distributions.kl_divergence(p, q)
+    return torch.mean(kl_loss)
+
+
+def Gaussian_js_loss(pred_mean, pred_std, gt_mean, gt_std):
+    p = torch.distributions.Normal(pred_mean, pred_std)
+    q = torch.distributions.Normal(gt_mean, gt_std)
+    p_q_kl = torch.distributions.kl_divergence(p, q)
+    q_p_kl = torch.distributions.kl_divergence(q, p)
+    js_loss = 0.5 * (p_q_kl + q_p_kl)
+    return torch.mean(js_loss)
